6e0d12d8b6e01d7f4c1e03d3e6e9f03e8dfd565b
T0nyBlue <18521123@gm.uit.edu.vn>
2021-10-05 18:37:05 +0700

    Add Filter function JS

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,25 @@
-let btns = document.querySelectorAll('.nav')
+let btns = document.querySelectorAll('.nav');
+let product_list = document.querySelectorAll('.item')
+// let filter = document.querySelectorAll('.active-filter');
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function() {
-        btns.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-    });
+        btns.forEach(btn => btn.classList.remove('active-filter'));
+        this.classList.add('active-filter');
+
+        let get_type = document.getElementsByClassName('active-filter')[0].innerText;
+        if(get_type == 'All'){
+            product_list.forEach(prd => prd.style.display = 'flex')
+        }else{
+            product_list.forEach(prd => {
+                if(prd.dataset.type == get_type){
+                    prd.style.display = 'flex';
+                }else{
+                    prd.style.display = 'none';
+                }
+            });
+        }
+    });    
 });
 
 
